a379755422e09e08359aa0e2fb6b6e9488e980f9
KUBWIMANA Patrick <52809972+Patrick875@users.noreply.github.com>
2023-11-24 13:49:01 +0200

    Update app.js

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(cors({origin:"https://resilient-belekoy-1df16d.netlify.app"}));
-app.options("*", cors({origin:"https://resilient-belekoy-1df16d.netlify.app"}));
+app.options("*", cors());
 
 app.use("/api/v1/", userRouter);
 app.use("/api/v1/attendance/", attendanceRouter);
